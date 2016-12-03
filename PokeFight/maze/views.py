c827e9f8b5b3d2897a17dd22e4a4b65eb40c2bf7
tmt514 <math.tmt514@gmail.com>
2016-12-03 15:09:08 -0500

    session and messages

--- PokeFight/maze/views.py
+++ PokeFight/maze/views.py
@@ -7,6 +7,10 @@ from django.template import loader
 from .models import Player, Room
 
 def index(request):
+    # 初始化訊息欄位
+    msg = []
+
+    ### 第一步：取得當前狀態
     player_id = request.session.get('player_id', None)
 
     # 取得玩家物件
@@ -16,15 +20,29 @@ def index(request):
         player.location_id = Room.objects.first().id
         player.save()
         player_id = player.id
+        request.session['player_id'] = player_id
+        msg.append("新玩家誕生了！")
     else:
         player = Player.objects.get(id=player_id)
         # TODO: handle exception
 
     # 取得地點物件
     room = player.location()
+    msg.append("獲得科科")
+
+
+
+
+    ### 第二步：根據目前的指令執行對應動作
+
+
+
+
+
 
     template = loader.get_template('maze/index.html')
     context = {
+        'msg': msg,
         'room': room,
         'player': player,
     }
