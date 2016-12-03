4aefe990220b27e0a3a92a5d558b271b1d9eddf6
Wei Lee <whus6207@gmail.com>
2016-12-03 15:51:09 -0500

    item action

--- PokeFight/maze/models.py
+++ PokeFight/maze/models.py
@@ -11,9 +11,6 @@ class Player(models.Model):
     def location(this):
         return Room.objects.get(id=this.location_id)
 
-    def get_items(this):
-        return PlayerItem.objects.get(player=this.id)
-
     class Action:
         name = ""
         key = ""
@@ -30,9 +27,9 @@ class Player(models.Model):
             ret.append(this.Action(name = "前往 " + x.name, key = "goto:" + str(x.id)))
 
         # 獲得 Item Actions
-        items = this.get_items()
-        for i in items:
-            ret.append(this.Action(name = "使用 " + i.item.name, key = "use:" + str(i.item.id)))
+        items = this.playeritem_set
+        #for i in items:
+        #    ret.append(this.Action(name = "使用 " + i.item.name, key = "use:" + str(i.item.id)))
         # 獲得 Monster Actions
 
         return ret
